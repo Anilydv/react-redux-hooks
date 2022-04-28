f1bc4a25dc3e6f883fa7c11be38148ddd4fef3e6
Anil Yadav <anilyadav@anils-mbp.lan>
2022-04-28 14:16:30 -0700

    first commit

---
+++ readMe/overviewofRedux.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function overviewofRedux() {
+  return (
+    <div>
+      when we want to change a state , we dispatch an action which triggers a
+      reducer function which in turn changes the state which is located in a
+      global store/state
+    </div>
+  );
+}
